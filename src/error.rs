3870bf574c14b95254eb57b3fe8ee84bf93af4d8
vladislav <ve@incsub.com>
2022-05-09 06:11:50 +0200

    Implement basic expression parser

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,19 @@
 #[derive(Debug)]
 pub enum Error {
-    ParseError(String),
+    ParseData(String),
+    ParseExpression(String),
     FilterError,
 }
 
 impl From<std::io::Error> for Error {
     fn from(e: std::io::Error) -> Self {
-        return Error::ParseError(format!("Unable to parse data because: {}", e));
+        return Error::ParseData(format!("Unable to parse data because: {}", e));
     }
 }
 
 impl From<json::Error> for Error {
     fn from(e: json::Error) -> Self {
-        return Error::ParseError(format!("Unable to parse data because: {}", e));
+        return Error::ParseData(format!("Unable to parse data because: {}", e));
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,69 @@ use filter::*;
 mod lexer;
 use lexer::*;
 
-fn main() -> Result<(), Error> {
-    let lex = Lexer::new("161 some tokens 1312 wat");
-    for token in lex {
-        println!("{:?}", token);
+fn parse(source: &str) -> Result<Vec<FilterType>, Error> {
+    let mut lex = Lexer::new(source).peekable();
+    let mut sequence= Vec::new();
+
+    let mut token = lex.next();
+    while token.is_some() {
+        match token {
+            Some(Token::Dot) => {
+                if let Some(t) = lex.peek() {
+                    match t {
+                        Token::Word(word) => {
+                            sequence.push(FilterType::Entry(word.to_string()));
+                            lex.next();
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Some(Token::OpenBracket) => {
+                token = lex.next();
+                if let Some(Token::CloseBracket) = token {
+                    sequence.push(FilterType::Array);
+                } else if let Some(Token::Number(num)) = token {
+                    token = lex.next();
+                    if let Some(Token::CloseBracket) = token {
+                        sequence.push(FilterType::Member(num.parse::<usize>().unwrap()));
+                    } else {
+                        return Err(Error::ParseExpression("expected close bracket after number".to_string()));
+                    }
+                } else if let Some(Token::Word(word)) = token {
+                    token = lex.next();
+                    if let Some(Token::CloseBracket) = token {
+                        sequence.push(FilterType::Entry(word));
+                    } else {
+                        return Err(Error::ParseExpression("expected close bracket after word".to_string()));
+                    }
+                } else {
+                    return Err(Error::ParseExpression("expected number, word or close bracket".to_string()));
+                }
+            }
+            Some(Token::Word(word)) => {
+                match word.as_str() {
+                    "keys" => sequence.push(FilterType::Keys),
+                    _ => return Err(Error::ParseExpression(format!("unknown keyword: {}", word))),
+                }
+            }
+            _ => {}
+        }
+        token = lex.next();
     }
+
+    Ok(sequence)
+}
+
+fn main() -> Result<(), Error> {
+    let filters = parse(".[]|keys")?;
+
+    let filename = "test-data/one.json";
+    let contents = std::fs::read_to_string(filename)?;
+    let data = json::parse(&contents)?;
+
+    let mut filter = Filter::new(filters);
+    println!("{:?}", filter.apply(data));
     Ok(())
 }
 
