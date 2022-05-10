753a9be45bf89b79152155d97a1f889259f38bdd
vladislav <ve@incsub.com>
2022-05-10 06:20:06 +0200

    Implement basic cli interface

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,35 @@ mod lexer;
 mod parser;
 use parser::*;
 
-fn main() -> Result<(), Error> {
-    let mut parser = ExpressionParser::new(".[0]|.name");
-    let filters = parser.parse()?;
-    println!("{:?}", filters);
+fn get_input(prompt: &str) -> Result<String, Error> {
+    use std::io::{stdin,stdout,Write};
+    let mut s = String::new();
+    print!("{} ", prompt);
+    stdout().flush()?;
+    stdin().read_line(&mut s)?;
 
+    if let Some('\n') = s.chars().next_back() { s.pop(); }
+    if let Some('\r') = s.chars().next_back() { s.pop(); }
+
+    Ok(s)
+}
+
+fn main() -> Result<(), Error> {
     let filename = "test-data/one.json";
     let contents = std::fs::read_to_string(filename)?;
     let data = json::parse(&contents)?;
 
-    let mut filter = Filter::new(filters);
-    println!("{:?}", filter.apply(data)?);
+    loop {
+        let input = get_input(">")?;
+        if input == "q" || input == "quit" {
+            break;
+        }
+        let mut parser = ExpressionParser::new(&input);
+        let filters = parser.parse()?;
+        println!("{:?}", filters);
+
+        let mut filter = Filter::new(filters);
+        println!("{:?}", filter.apply(data.clone())?);
+    }
     Ok(())
 }
